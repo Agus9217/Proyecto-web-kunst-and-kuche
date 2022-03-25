0ceaa2db4e2aa40a1f1a7b4c3735194ac10a0e8a
Agustin Ibarrola <agustinibarrola@Just1n.lan>
2022-03-24 23:31:25 -0300

    terminando

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ function box2Scroll() {
     for(var i=0; i < animadoBox2.length; i++ ) {
         let alturaAnimadoBox2 = animadoBox2[i].offsetTop;
         if(alturaAnimadoBox2 - 750 < scrollTop) {
-            animadoBox2[i].className = "row animate__animated animate__backInUp";
+            animadoBox2[i].className = "col-md-4 my-3 animate__animated animate__backInUp";
         }
     }
 }
 
 
-/*function box1Scroll() {
+function box1Scroll() {
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i < animadoBox1.length; i++ ) {
         let alturaAnimadoBox1 = animadoBox1[i].offsetTop;
-        if(alturaAnimadoBox1 - 600 < scrollTop) {
-            animadoBox1[i].className = "col-md-6 animate__animated animate__fadeInDown";
+        if(alturaAnimadoBox1 - 750 < scrollTop) {
+            animadoBox1[i].className = "row align-items-md-stretch animate__animated animate__backInLeft";
         }
     }
-}*/
+}
 
 
 
-window.onscroll = function() {/*box1Scroll(),*/ box2Scroll()};
+window.onscroll = function() {box1Scroll(), box2Scroll()};
