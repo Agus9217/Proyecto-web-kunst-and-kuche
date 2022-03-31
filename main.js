0361f5346c8bd6584545cfe3025d9523165ba205
Agustin Ibarrola <agustinibarrola@Just1n.lan>
2022-03-31 01:24:18 -0300

    version-v11

--- main.js
+++ main.js
@@ -33,6 +33,17 @@ function box3Scroll() {
     }
 }
 
+function colorScroll(){
+    if(document.documentElement.scrollTop > 420){
+        document.getElementById("navBar").className = "navbar navbar-expand-lg navbar-dark bg-black fixed-top";
+    }
+    else {document.getElementById("navBar").className = "navbar navbar-expand-lg navbar-dark fixed-top"}
+}
 
 
-window.onscroll = function() {box1Scroll(), box2Scroll(), box3Scroll()};
+
+
+
+
+
+window.onscroll = function() {box1Scroll(), box2Scroll(), box3Scroll(), colorScroll()};
