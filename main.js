5ce8898401e19ae99b063e418fb27718b41fcf98
Agustin Ibarrola <agustinibarrola@Just1n.lan>
2022-04-10 22:30:18 -0300

    v30

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function box2Scroll() {
     for(var i=0; i < animadoBox2.length; i++ ) {
         let alturaAnimadoBox2 = animadoBox2[i].offsetTop;
         if(alturaAnimadoBox2 - 750 < scrollTop) {
-            animadoBox2[i].className = "col-md-4 my-3 animate__animated animate__backInUp";
+            animadoBox2[i].className = "col-md-4 mt-3 mt-md-5 animate__animated animate__backInUp";
         }
     }
 }
@@ -18,7 +18,7 @@ function box1Scroll() {
     for(var i=0; i < animadoBox1.length; i++ ) {
         let alturaAnimadoBox1 = animadoBox1[i].offsetTop;
         if(alturaAnimadoBox1 - 750 < scrollTop) {
-            animadoBox1[i].className = "row mx-auto animate__animated animate__backInLeft";
+            animadoBox1[i].className = "row mx-auto mt-md-5 animate__animated animate__backInLeft";
         }
     }
 }
@@ -28,7 +28,7 @@ function box3Scroll() {
     for(var i=0; i < animadoBox3.length; i++ ) {
         let alturaAnimadoBox3 = animadoBox3[i].offsetTop;
         if(alturaAnimadoBox3 - 750 < scrollTop) {
-            animadoBox3[i].className = "row align-items-md-stretch animate__animated animate__backInLeft";
+            animadoBox3[i].className = "animate__animated animate__backInLeft";
         }
     }
 }
